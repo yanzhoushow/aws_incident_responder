c1981ae8da90951e3d2444f674b0cb7d72ae2b4c
Yan Zhou <38987117+yanzhoushow@users.noreply.github.com>
2020-04-10 05:47:33 -0400

    add db name

--- athena_query.sql
+++ athena_query.sql
@@ -3,7 +3,7 @@ WITH events AS
             min(eventtime) OVER (PARTITION BY useridentity.accountid) AS min_event_timestamp, 
             max(eventtime) OVER (PARTITION BY useridentity.accountid) AS max_event_timestamp, 
             count(*) OVER (PARTITION BY useridentity.accountid) AS event_count
-    FROM cloudtrail
+    FROM flaws2.cloudtrail
     WHERE useridentity.accountid IS NOT NULL)
 SELECT DISTINCT account_id,
        DATE_DIFF('second', from_iso8601_timestamp(min_event_timestamp), from_iso8601_timestamp(max_event_timestamp))*1.0/(event_count-1) AS mean_interval_in_second
