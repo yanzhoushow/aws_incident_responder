18928a626e67578c8455e07278c2a7816e1eb915
Yan Zhou <38987117+yanzhoushow@users.noreply.github.com>
2020-04-10 05:42:55 -0400

    minor format updates

--- athena_query.sql
+++ athena_query.sql
@@ -1,10 +1,10 @@
 WITH events AS 
     (SELECT useridentity.accountid AS account_id,
-            min(eventtime) OVER (partition by useridentity.accountid) AS min_event_timestamp, 
-            max(eventtime) OVER (partition by useridentity.accountid) AS max_event_timestamp, 
-            count(*) OVER (partition by useridentity.accountid) AS event_count
+            min(eventtime) OVER (PARTITION BY useridentity.accountid) AS min_event_timestamp, 
+            max(eventtime) OVER (PARTITION BY useridentity.accountid) AS max_event_timestamp, 
+            count(*) OVER (PARTITION BY useridentity.accountid) AS event_count
     FROM cloudtrail
-    WHERE useridentity.accountid is NOT NULL)
+    WHERE useridentity.accountid IS NOT NULL)
 SELECT DISTINCT account_id,
-       date_diff('second', from_iso8601_timestamp(min_event_timestamp), from_iso8601_timestamp(max_event_timestamp))*1.0/(event_count-1) AS mean_interval_in_second
+       DATE_DIFF('second', from_iso8601_timestamp(min_event_timestamp), from_iso8601_timestamp(max_event_timestamp))*1.0/(event_count-1) AS mean_interval_in_second
 FROM events
